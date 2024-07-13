4e76b311d585082b4cf401a1c9709c61a1626c19
NICE-97 <pattanawongsangpan@gmail.com>
2024-07-13 22:35:57 +0700

    first commit

---
+++ src/components/Score.js
@@ -0,0 +1,19 @@
+import { useContext } from "react"
+import { DataContext } from "../App"
+import QuestionsData from "../data/QuestionsData";
+
+const Score = ()=>{
+    const {score, setAppState, setScore} = useContext(DataContext);
+    const restartApp = ()=>{
+        setAppState('menu');
+        setScore(0);
+    }
+    return(
+        <div className="Score">
+            <h1>สรุปผลคะแนนสอบ</h1>
+            <h2>{score} / {QuestionsData.length}</h2>
+            <button onClick={()=>restartApp()}>ทำแบบทดสอบอีกครั้ง</button>
+        </div>
+    )
+}
+export default Score
